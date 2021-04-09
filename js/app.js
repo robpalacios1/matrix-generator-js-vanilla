0d1940c0e12016adc45f2f6132dcef510577ccfb
roberto <robpalacios1@hotmail.com>
2021-04-08 20:11:54 -0500

    add rows and columns

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const dataButton = document.querySelector('.js-data-button');
 const arrayContainer = document.querySelector('.js-array-container');
-
+let inputN = 0;
+let inputM = 0;
 
 
 dataButton.addEventListener('click', () => {
-  const inputN = Number(document.querySelector('.js-input-n-row').value);
-  const inputM = Number(document.querySelector('.js-input-m-row').value);
-
+  inputN = Number(document.querySelector('.js-input-n-row').value);
+  inputM = Number(document.querySelector('.js-input-m-col').value);
   arrayContainer.innerHTML = '';
+  generateCol(inputN, inputM);
+
+});
+
+//***************************************
+
+function generateCol(row, col) {
+
+  for (let i = 0; i < row; i++) {
+    
   /* <div class="array__row"> */
   const arrayRow = document.createElement("div");
   arrayRow.className = "array";
@@ -20,20 +30,44 @@ dataButton.addEventListener('click', () => {
   arrayRow.appendChild(generateRow(inputM));
   arrayRow.appendChild(span);
   arrayContainer.appendChild(arrayRow);
-});
 
-// **************************************
-function generateArray() {
-  return;
-}
+  }
 
-
-// **************************************
-function generateRow(col) {
 
   /* <div class="array__row"> */
-  const row = document.createElement("div");
-  row.className = "array__row";
+  const arrayRow = document.createElement("div");
+  arrayRow.className = "array";
+
+
+  for (let j = 0; j < col; j++) {
+
+  /* Create span item */
+  const span = document.createElement("span");
+  span.className = "array__span indent";
+  span.innerText = "100";
+
+  arrayRow.appendChild(span);
+  }
+
+  arrayContainer.appendChild(arrayRow);
+
+
+  
+
+
+
+
+
+}
+
+// **************************************
+
+function generateRow(col) {
+
+
+  /* <div class="array__row"> */
+  const arrayRow = document.createElement("div");
+  arrayRow.className = "array__row";
 
   for (let i = 0; i < col; i++) {
 
@@ -42,16 +76,8 @@ function generateRow(col) {
     input.className = "array__row__col";
     input.type = "text";
 
-    row.appendChild(input);
+    arrayRow.appendChild(input);
   }
 
-  return row;
-
+  return arrayRow;
 }
-
-
-
-
-
-
-
