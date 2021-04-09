dafec42b8e58c1dc23e2797116aa22348231564a
roberto <robpalacios1@hotmail.com>
2021-04-09 10:37:05 -0500

    validate only allow number into inputs

--- js/app.js
+++ js/app.js
@@ -5,30 +5,28 @@ let inputN = 0;
 let inputM = 0;
 
 
+//event click to refer a value into a input
 dataButton.addEventListener('click', () => {
   inputN = Number(document.querySelector('.js-input-n-row').value);
   inputM = Number(document.querySelector('.js-input-m-col').value);
   arrayContainer.innerHTML = '';
 
+  // cicle for to obtain the value into an object
   for (let i = 0; i < inputN; i++) {
     myArray[i+1] = {};
     for (let j = 0; j < inputM; j++) {
       myArray[i+1][j+1] = 0;
     }
   }
-
-
-
   generateCol(inputN, inputM);
-
 });
 
-//***************************************
+//******Function to create a Col*******
 
 function generateCol(row, col) {
 
   for (let i = 0; i < row; i++) {
-    
+
   /* <div class="array__row"> */
   const arrayRow = document.createElement("div");
   arrayRow.className = "array";
@@ -41,15 +39,8 @@ function generateCol(row, col) {
   arrayRow.appendChild(generateRow(i, inputM));
   arrayRow.appendChild(span);
   arrayContainer.appendChild(arrayRow);
-
   }
 
-
-
-
-
-
-  
   /* <div class="array__row"> */
   const arrayRow = document.createElement("div");
   arrayRow.className = "array";
@@ -64,29 +55,24 @@ function generateCol(row, col) {
 
   arrayRow.appendChild(span);
   }
-
   arrayContainer.appendChild(arrayRow);
-
-
-  
-
-
-
-
-
 }
 
-// **************************************
+// *****Function to allow only numbers*****
 
-function isInputNumber(evt) {
-  let char = String.fromCharCode(evt.which);
-
-  if(!(/[0-9]/.test(char))) {
-    evt.preventDefault();
+function isInputNumber(e) {
+  let regularExpresion = new RegExp('^[1-9]?[0-9]{1}$|^100$');
+  let regExpOk = regularExpresion.test(e.target.value);
+  if ( !regExpOk && (e.target.value != '') ){
+    e.preventDefault();
+    e.target.value = '';
+    return false;
+  } else {
+    return true;
   }
 }
 
-// **************************************
+// *******Function to create a Row*******
 
 function generateRow(row, col) {
 
@@ -94,7 +80,7 @@ function generateRow(row, col) {
   /* <div class="array__row"> */
   const arrayRow = document.createElement("div");
   arrayRow.className = "array__row";
-
+  
   for (let i = 0; i < col; i++) {
 
     /* input */
@@ -103,35 +89,32 @@ function generateRow(row, col) {
     input.type = "text";
     input.dataset.idrow = row + 1;
     input.dataset.idcol = i + 1;
-
-    arrayRow.appendChild(input);
+    
+    arrayRow.appendChild(input)
 
     input.addEventListener('input', (e)=>{
-
-      changeRow = Number(e.target.dataset.idrow);
-      changeCol = Number(e.target.dataset.idcol);
-
-      myArray[changeRow][changeCol] = Number(e.target.value);
-      console.log(myArray);
-
-      let sumCol = 0;
-      let sumRow = 0;
-
-      for (let i = 1; i <= inputM; i++ ) {
-        sumCol += myArray[changeRow][i];
+      if (isInputNumber(e)) {
+        changeRow = Number(e.target.dataset.idrow);
+        changeCol = Number(e.target.dataset.idcol);
+  
+        myArray[changeRow][changeCol] = Number(e.target.value);
+        console.log(myArray);
+  
+        let sumCol = 0;
+        let sumRow = 0;
+  
+        for (let i = 1; i <= inputM; i++ ) {
+          sumCol += myArray[changeRow][i];
+        }
+  
+        for (let j = 1; j <= inputN; j++ ) {
+          sumRow += myArray[j][changeCol];
+        }
+  
+        document.querySelector(`.js-span-n${changeRow}`).innerText = sumCol;
+        document.querySelector(`.js-span-m${changeCol}`).innerText = sumRow;
       }
-
-      for (let j = 1; j <= inputN; j++ ) {
-        sumRow += myArray[j][changeCol];
-      }
-
-      document.querySelector(`.js-span-n${changeRow}`).innerText = sumCol;
-      document.querySelector(`.js-span-m${changeCol}`).innerText = sumRow;
-
     })
   }
-
   return arrayRow;
 }
-
-
